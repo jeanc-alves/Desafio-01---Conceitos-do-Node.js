065dc29df5618626a7d3f1667e39463476b9a074
Jean Alves <jean.alves@inovan.do>
2021-07-13 08:43:48 -0300

    chore(desafio): initial commit

--- src/index.js
+++ src/index.js
@@ -1,41 +1,87 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+  
+  if(!user){
+    return response.status(400).json({erro: "User not Found"})
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+  users.push({
+    id: uuidv4(), name, username, todos: [],
+})
+  response.status(201).send(users)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request
+  const {title, deadline} = request.body
+
+  const createTodo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline), 
+    created_at: new Date(), 
+  }
+
+  user.todos.push(createTodo)
+
+  return response.status(201).send(createTodo)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params
+  const {user} = request
+  const { title, deadline} = request.body
+
+  const selectedTodo = user.todos.find(todo => todo.id === id);
+  selectedTodo.title = title
+  selectedTodo.deadline = deadline
+
+  response.status(201).send(selectedTodo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params
+  const {user} = request
+  const { done } = request.body
+
+  const selectedTodo = user.todos.find(todo => todo.id === id);
+  selectedTodo.done = done
+
+  response.status(201).send(selectedTodo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {id} = request.params
+  const {user} = request
+
+  const selectedTodo = user.todos.find(todo => todo.id === id);
+  user.todos.splice(user.todos, 1)
+  return response.status(200).json(selectedTodo).send()
 });
 
 module.exports = app;
